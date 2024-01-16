923f2e1eb209f6604f3decedd1d1ed428eeb1fbc
“Denys” <denys.khmelnytskyi@nthrive.com>
2024-01-16 20:44:26 +0100

    tf fmt

--- main.tf
+++ main.tf
@@ -1,55 +1,55 @@
 resource "azurerm_resource_group" "rg" {
-    location = "westeurope"
-    name = "static-web-rg"
+  location = "westeurope"
+  name     = "static-web-rg"
 }
 
 resource "random_string" "sa_name" {
-    length = 8
-    lower = true
-    numeric = false
-    special = false
-    upper = false
+  length  = 8
+  lower   = true
+  numeric = false
+  special = false
+  upper   = false
 }
 
 resource "azurerm_storage_account" "sa" {
-    resource_group_name = azurerm_resource_group.rg.name
-    location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
 
-    name = random_string.sa_name.result
+  name = random_string.sa_name.result
 
-    account_tier = "Standard"
-    account_replication_type = "LRS"
-    account_kind = "StorageV2"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
 
-    static_website{
-        index_document = "index.html"
-    }
+  static_website {
+    index_document = "index.html"
+  }
 }
 
 resource "azurerm_storage_blob" "blob" {
-    name = "index.html"
-    storage_account_name = azurerm_storage_account.sa.name
-    storage_container_name = "$web"
-    type = "Block"
-    content_type = "text/html"
+  name                   = "index.html"
+  storage_account_name   = azurerm_storage_account.sa.name
+  storage_container_name = "$web"
+  type                   = "Block"
+  content_type           = "text/html"
 }
 
 resource "azurerm_cdn_profile" "cdn_profile" {
-    name = "devops01-cdn"
-    location = azurerm_resource_group.rg.location
-    resource_group_name = azurerm_resource_group.rg.name
-    sku = "Standard_Microsoft"
+  name                = "devops01-cdn"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "Standard_Microsoft"
 }
 
 resource "azurerm_cdn_endpoint" "cdn_endpoint" {
-    name = "devops01-endpoint"
-    profile_name = azurerm_cdn_profile.cdn_profile.name
-    location = azurerm_resource_group.rg.location
-    resource_group_name = azurerm_resource_group.rg.name
-    origin_host_header = azurerm_storage_account.sa.primary_web_host
+  name                = "devops01-endpoint"
+  profile_name        = azurerm_cdn_profile.cdn_profile.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  origin_host_header  = azurerm_storage_account.sa.primary_web_host
 
-    origin{
-        name = "HelloThere"
-        host_name = azurerm_storage_account.sa.primary_web_host
-    }
+  origin {
+    name      = "HelloThere"
+    host_name = azurerm_storage_account.sa.primary_web_host
+  }
 }
--- provider.tf
+++ provider.tf
@@ -1,23 +1,23 @@
 terraform {
-    required_providers {
-        azurerm ={
-            source = "hashicorp/azurerm"
-            version = "~>3.0"
-        }
-        random={
-            source = "hashicorp/random"
-            version = "~>3.0"            
-        }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
     }
-    backend "azurerm"{
-        resource_group_name = "tfstate-rg"
-        storage_account_name = "tfstate8989"
-        container_name = "tfstate"
-        key = "terraform.tfstate"
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
     }
+  }
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "tfstate8989"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
-    skip_provider_registration = true
-    features{}
+  skip_provider_registration = true
+  features {}
 }
